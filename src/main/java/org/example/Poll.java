5941e41d79785f6e7345d407c0804193c9646919
ori162 <oriaharon1245@gmail.com>
2025-07-13 17:25:09 +0300

    in PollItem i've done getter & setter & add function that check if have place to put in more answer or not. in the Poll class, it gets array of PollItem & currentSize that helps to use tha ADD function. in constructor have variable int to help to do restriction from the start about how many questions make.

--- src/main/java/org/example/Poll.java
+++ src/main/java/org/example/Poll.java
@@ -1,4 +1,38 @@
 package org.example;
 
 public class Poll {
+    private PollItem[] questions;
+    private int currentSize;
+
+    public Poll(int maxSize){
+        this.questions = new PollItem[maxSize];
+        this.currentSize = 0;
+    }
+
+    public boolean addQuestion(PollItem item){
+        if(this.currentSize < this.questions.length){
+            this.questions[this.currentSize] = item;
+            this.currentSize++;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public PollItem[] getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(PollItem[] questions) {
+        this.questions = questions;
+    }
+
+    public int getCurrentSize() {
+        return currentSize;
+    }
+
+    public void setCurrentSize(int currentSize) {
+        this.currentSize = currentSize;
+    }
 }
--- src/main/java/org/example/PollItem.java
+++ src/main/java/org/example/PollItem.java
@@ -1,5 +1,41 @@
 package org.example;
 
 public class PollItem {
+    private String question;
+    private String[] answer;
+    private int currentAnswerIndex;
 
+
+    public PollItem(String questions) {
+        this.question = questions;
+        this.answer = new String[3];
+        this.currentAnswerIndex = 0;
+
+    }
+
+    public boolean addAnswer(String answer) {
+        if (this.currentAnswerIndex < this.answer.length) {
+            this.answer[this.currentAnswerIndex] = answer;
+            this.currentAnswerIndex++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String[] getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String[] answer) {
+        this.answer = answer;
+    }
 }
